cc73fc913e03c5c848c5041d8c01131738f67a07
Kevin.Tung <chen0625tung@gmail.com>
2021-06-15 02:50:14 +0800

    remove unwanted files and added PrintIntroduction function

--- main.cpp
+++ main.cpp
@@ -3,41 +3,119 @@
 #include <sstream>
 #include <algorithm>
 
-int main()
+void PrintIntroduction()
 {
-    std::cout << "You are a secret agent hacking into a secure server room..." << std::endl;
-    std::cout << "beginning security level " << "1" << "..." << std::endl;
-    std::cout << "You need to enter the correct codes to continue..." << std::endl;
+    /*
+                          _____                          
+                   _.+sd$$$$$$$$$bs+._                   
+               .+d$$$$$$$$$$$$$$$$$$$$$b+.               
+            .sd$$$$$$$P^*^T$$$P^*"*^T$$$$$bs.            
+          .s$$$$$$$$P*     `*' _._  `T$$$$$$$s.          
+        .s$$$$$$$$$P          ` :$;   T$$$$$$$$s.        
+       s$$$$$$$$$$;  db..+s.   `**'    T$$$$$$$$$s       
+     .$$$$$$$$$$$$'  `T$P*'             T$$$$$$$$$$.     
+    .$$$$$$$$$$$$P                       T$$$$$$$$$$.    
+   .$$$$$$$$$$$$$b                       `$$$$$$$$$$$.   
+  :$$$$$$$$$$$$$$$.                       T$$$$$$$$$$$;  
+  $$$$$$$$$P^*' :$$b.                     d$$$$$$$$$$$$  
+ :$$$$$$$P'      T$$$$bs._               :P'`*^T$$$$$$$; 
+ $$$$$$$P         `*T$$$$$b              '      `T$$$$$$ 
+:$$$$$$$b            `*T$$$s                      :$$$$$;
+:$$$$$$$$b.                                        $$$$$;
+$$$$$$$$$$$b.                                     :$$$$$$
+$$$$$$$$$$$$$bs.                                 .$$$$$$$
+$$$$$$$$$$$$$$$$$bs.                           .d$$$$$$$$
+:$$$$$$$$$$$$$P*"*T$$bs,._                  .sd$$$$$$$$$;
+:$$$$$$$$$$$$P     TP^**T$bss++.._____..++sd$$$$$$$$$$$$;
+ $$$$$$$$$$$$b           `T$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 
+ :$$$$$$$$$$$$b.           `*T$$P^*"*"*^^*T$$$$$$$$$$$$; 
+  $$$b       `T$b+                        :$$$$$$$BUG$$  
+  :$P'         `"'               ,._.     ;$$$$$$$$$$$;  
+   \                            `*TP*     d$$P*******$   
+    \                                    :$$P'      /    
+     \                                  :dP'       /     
+      `.                               d$P       .'      
+       `.                             `'      .'        
+          `-.                               .-'          
+             `-.                         .-'             
+                `*+-._             _.-+*'                
+                      `"*-------*"'
+    */
+    const char *art = 
+    "                          _____                          \n"
+    "                   _.+sd$$$$$$$$$bs+._                   \n"
+    "               .+d$$$$$$$$$$$$$$$$$$$$$b+.               \n"
+    "            .sd$$$$$$$P^*^T$$$P^*\"*^T$$$$$bs.            \n"
+    "          .s$$$$$$$$P*     `*' _._  `T$$$$$$$s.          \n"
+    "        .s$$$$$$$$$P          ` :$;   T$$$$$$$$s.        \n"
+    "       s$$$$$$$$$$;  db..+s.   `**'    T$$$$$$$$$s       \n"
+    "     .$$$$$$$$$$$$'  `T$P*'             T$$$$$$$$$$.     \n"
+    "    .$$$$$$$$$$$$P                       T$$$$$$$$$$.    \n"
+    "   .$$$$$$$$$$$$$b                       `$$$$$$$$$$$.   \n"
+    "  :$$$$$$$$$$$$$$$.                       T$$$$$$$$$$$;  \n"
+    "  $$$$$$$$$P^*' :$$b.                     d$$$$$$$$$$$$  \n"
+    " :$$$$$$$P'      T$$$$bs._               :P'`*^T$$$$$$$; \n"
+    " $$$$$$$P         `*T$$$$$b              '      `T$$$$$$ \n"
+    ":$$$$$$$b            `*T$$$s                      :$$$$$;\n"
+    ":$$$$$$$$b.                                        $$$$$;\n"
+    "$$$$$$$$$$$b.                                     :$$$$$$\n"
+    "$$$$$$$$$$$$$bs.                                 .$$$$$$$\n"
+    "$$$$$$$$$$$$$$$$$bs.                           .d$$$$$$$$\n"
+    ":$$$$$$$$$$$$$P*\"*T$$bs,._                  .sd$$$$$$$$$;\n"
+    ":$$$$$$$$$$$$P     TP^**T$bss++.._____..++sd$$$$$$$$$$$$;\n"
+    " $$$$$$$$$$$$b           `T$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ \n"
+    " :$$$$$$$$$$$$b.           `*T$$P^*\"*\"*^^*T$$$$$$$$$$$$; \n"
+    "  $$$b       `T$b+                        :$$$$$$$BUG$$  \n"
+    "  :$P'         `\"'               ,._.     ;$$$$$$$$$$$;  \n"
+    "   \\                            `*TP*     d$$P*******$   \n"
+    "    \\                                    :$$P'      /    \n"
+    "     \\                                  :dP'       /     \n"
+    "      `.                               d$P       .'      \n"
+    "       `.                             `'      .'        \n"
+    "          `-.                               .-'          \n"
+    "             `-.                         .-'             \n"
+    "                `*+-._             _.-+*'                \n"
+    "                      `\"*-------*\"'\n"
+    ;
+    std::cout << art << std::endl;
+    std::cout << "You are a secret agent hacking into a secure server room...\n";
+    std::cout << "beginning security level " << "1" << "...\n";
+    std::cout << "You need to enter the correct codes to continue...\n\n";
+}
 
-    const int a = 5;
-    const int b = 6;
-    const int c = 7;
+void PlayGame()
+{
+    PrintIntroduction();
+    const int CodeA = 5;
+    const int CodeB = 6;
+    const int CodeC = 7;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The 3 numbers add up to: " << sum << std::endl;
-    std::cout << "+ The 3 numbers multiply up to: " << product << std::endl;
+    std::cout << "+ There are 3 numbers in the code";
+    std::cout << "\n+ The 3 numbers add up to: " << CodeSum;
+    std::cout << "\n+ The 3 numbers multiply up to: " << CodeProduct;
 
-    std::cout << "what are the 3 number code?" << std::endl;
+    std::cout << "\nwhat are the 3 number code?\n";
 
-    std::string code;
-    std::stringstream ans;
-    getline(std::cin, code);
-    std::sort(code.begin(), code.end());
+    int GuessA, GuessB, GuessC, GuessSum, GuessProduct;
+    std::cin >> GuessA >> GuessB >> GuessC;
+    GuessSum = GuessA + GuessB + GuessC;
+    GuessProduct = GuessA * GuessB * GuessC;
 
-    ans << c << a << b;
-    std::string sortedAns = ans.str();
-    std::sort(sortedAns.begin(), sortedAns.end());
-    if(sortedAns == code)
+    if(GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "Congradulations! you got it right!" << std::endl;
+        std::cout << "You Win!\n";
     }
     else
     {
-        std::cout << "Wrong! guess again!" << std::endl;
+        std::cout << "Wrong Guess, You lose...\n";
     }
+}
+
+int main()
+{
+    PlayGame();
     return 0;
 }
--- self_attempt_tripleX.cpp
+++ self_attempt_tripleX.cpp
@@ -4,6 +4,86 @@
 #include <algorithm>
 #include <ctime>
 
+void PrintIntroduction(int level)
+{
+    /*
+                          _____                          
+                   _.+sd$$$$$$$$$bs+._                   
+               .+d$$$$$$$$$$$$$$$$$$$$$b+.               
+            .sd$$$$$$$P^*^T$$$P^*"*^T$$$$$bs.            
+          .s$$$$$$$$P*     `*' _._  `T$$$$$$$s.          
+        .s$$$$$$$$$P          ` :$;   T$$$$$$$$s.        
+       s$$$$$$$$$$;  db..+s.   `**'    T$$$$$$$$$s       
+     .$$$$$$$$$$$$'  `T$P*'             T$$$$$$$$$$.     
+    .$$$$$$$$$$$$P                       T$$$$$$$$$$.    
+   .$$$$$$$$$$$$$b                       `$$$$$$$$$$$.   
+  :$$$$$$$$$$$$$$$.                       T$$$$$$$$$$$;  
+  $$$$$$$$$P^*' :$$b.                     d$$$$$$$$$$$$  
+ :$$$$$$$P'      T$$$$bs._               :P'`*^T$$$$$$$; 
+ $$$$$$$P         `*T$$$$$b              '      `T$$$$$$ 
+:$$$$$$$b            `*T$$$s                      :$$$$$;
+:$$$$$$$$b.                                        $$$$$;
+$$$$$$$$$$$b.                                     :$$$$$$
+$$$$$$$$$$$$$bs.                                 .$$$$$$$
+$$$$$$$$$$$$$$$$$bs.                           .d$$$$$$$$
+:$$$$$$$$$$$$$P*"*T$$bs,._                  .sd$$$$$$$$$;
+:$$$$$$$$$$$$P     TP^**T$bss++.._____..++sd$$$$$$$$$$$$;
+ $$$$$$$$$$$$b           `T$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ 
+ :$$$$$$$$$$$$b.           `*T$$P^*"*"*^^*T$$$$$$$$$$$$; 
+  $$$b       `T$b+                        :$$$$$$$BUG$$  
+  :$P'         `"'               ,._.     ;$$$$$$$$$$$;  
+   \                            `*TP*     d$$P*******$   
+    \                                    :$$P'      /    
+     \                                  :dP'       /     
+      `.                               d$P       .'      
+       `.                             `'      .'        
+          `-.                               .-'          
+             `-.                         .-'             
+                `*+-._             _.-+*'                
+                      `"*-------*"'
+    */
+    const char *art = 
+    "                          _____                          \n"
+    "                   _.+sd$$$$$$$$$bs+._                   \n"
+    "               .+d$$$$$$$$$$$$$$$$$$$$$b+.               \n"
+    "            .sd$$$$$$$P^*^T$$$P^*\"*^T$$$$$bs.            \n"
+    "          .s$$$$$$$$P*     `*' _._  `T$$$$$$$s.          \n"
+    "        .s$$$$$$$$$P          ` :$;   T$$$$$$$$s.        \n"
+    "       s$$$$$$$$$$;  db..+s.   `**'    T$$$$$$$$$s       \n"
+    "     .$$$$$$$$$$$$'  `T$P*'             T$$$$$$$$$$.     \n"
+    "    .$$$$$$$$$$$$P                       T$$$$$$$$$$.    \n"
+    "   .$$$$$$$$$$$$$b                       `$$$$$$$$$$$.   \n"
+    "  :$$$$$$$$$$$$$$$.                       T$$$$$$$$$$$;  \n"
+    "  $$$$$$$$$P^*' :$$b.                     d$$$$$$$$$$$$  \n"
+    " :$$$$$$$P'      T$$$$bs._               :P'`*^T$$$$$$$; \n"
+    " $$$$$$$P         `*T$$$$$b              '      `T$$$$$$ \n"
+    ":$$$$$$$b            `*T$$$s                      :$$$$$;\n"
+    ":$$$$$$$$b.                                        $$$$$;\n"
+    "$$$$$$$$$$$b.                                     :$$$$$$\n"
+    "$$$$$$$$$$$$$bs.                                 .$$$$$$$\n"
+    "$$$$$$$$$$$$$$$$$bs.                           .d$$$$$$$$\n"
+    ":$$$$$$$$$$$$$P*\"*T$$bs,._                  .sd$$$$$$$$$;\n"
+    ":$$$$$$$$$$$$P     TP^**T$bss++.._____..++sd$$$$$$$$$$$$;\n"
+    " $$$$$$$$$$$$b           `T$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ \n"
+    " :$$$$$$$$$$$$b.           `*T$$P^*\"*\"*^^*T$$$$$$$$$$$$; \n"
+    "  $$$b       `T$b+                        :$$$$$$$BUG$$  \n"
+    "  :$P'         `\"'               ,._.     ;$$$$$$$$$$$;  \n"
+    "   \\                            `*TP*     d$$P*******$   \n"
+    "    \\                                    :$$P'      /    \n"
+    "     \\                                  :dP'       /     \n"
+    "      `.                               d$P       .'      \n"
+    "       `.                             `'      .'        \n"
+    "          `-.                               .-'          \n"
+    "             `-.                         .-'             \n"
+    "                `*+-._             _.-+*'                \n"
+    "                      `\"*-------*\"'\n"
+    ;
+    std::cout << art << std::endl;
+    std::cout << "You are a secret agent hacking into a secure server room...\n";
+    std::cout << "beginning security level " << level << "...\n";
+    std::cout << "You need to enter the correct codes to continue...\n\n";
+}
+
 std::string generateRandomNumber()
 {
     srand((unsigned) time(0));
@@ -42,23 +122,21 @@ void tripleXMainGame(int maxLevel)
     int initLevel = 1;
     while(initLevel < maxLevel)
     {
+        PrintIntroduction(initLevel);
         initLevel++;
-        std::cout << "You are a secret agent hacking into a secure server room..." << std::endl;
-        std::cout << "beginning security level " << initLevel << "..." << std::endl;
-        std::cout << "You need to enter the correct codes to continue..." << std::endl;
 
         std::string number = generateRandomNumber();
         const int CodeA = number[0] - '0';
         const int CodeB = number[1] - '0';
         const int CodeC = number[2] - '0';
 
-        const int sum = CodeA + CodeB + CodeC;
-        const int product = CodeA * CodeB * CodeC;
+        const int CodeSum = CodeA + CodeB + CodeC;
+        const int CodeProduct = CodeA * CodeB * CodeC;
 
         std::cout << std::endl;
         std::cout << "+ There are 3 numbers in the code" << std::endl;
-        std::cout << "+ The 3 numbers add up to: " << sum << std::endl;
-        std::cout << "+ The 3 numbers multiply up to: " << product << std::endl;
+        std::cout << "+ The 3 numbers add up to: " << CodeSum << std::endl;
+        std::cout << "+ The 3 numbers multiply up to: " << CodeProduct << std::endl;
 
         std::cout << "what are the 3 number code?" << std::endl;
 
